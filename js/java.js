ba5696b414c1e4e9c0cf7ffc7a7d9de63ebb78af
sanyakushpita <kushpita.o.com>
2023-07-15 12:53:10 +0300

    task77

--- js/java.js
+++ js/java.js
@@ -1,11 +1,11 @@
 // ЗАДАЧА 7
 // НАПИШІТЬ ПРОГРАМУ, ЯКА ПРОВІРЯЄ ЛОГІН І ПАРОЛЬ, І ВИВОДИТЬ ПРИВІТАННЯ - ЯКЩО ВСЕ ДОБРЕ, АБО ВИДАЄ ПОМИЛКУ , ЩО НЕ ВІРНИЙ ЛОГІН
 
-const login = 'sasha';
-const password = 12345;
+// const login = 'sasha';
+// const password = 12345;
 
-const userLogin = prompt('Введіть логін');
-const userPassword = Number(prompt('Введіть пароль'));
+// const userLogin = prompt('Введіть логін');
+// const userPassword = Number(prompt('Введіть пароль'));
 
 // if (userLogin === login && password === userPassword) {
 //   console.log('Вірно');
@@ -13,6 +13,6 @@ const userPassword = Number(prompt('Введіть пароль'));
 //   console.log('Помилка');
 // }
 
-const resolt =
-  userLogin === login && userPassword === password ? 'Вірно' : 'Помилка';
-console.log(resolt);
+// const resolt =
+//   userLogin === login && userPassword === password ? 'Вірно' : 'Помилка';
+// console.log(resolt);
